d54eb1c41c5ae3f06ec726dbec9d072710191640
zzzz7913 <791351885@qq.com>
2025-09-30 11:07:40 +0800

    完善实体类 GetGmvMaxReportResponse to map full JSON structure

---
+++ GetGmvMaxReportResponse.java
@@ -0,0 +1,28 @@
+// Assuming this is the content of GetGmvMaxReportResponse.java
+
+package com.example;
+
+public class GetGmvMaxReportResponse {
+    // Define fields according to the JSON structure
+    private String field1;
+    private int field2;
+    // Add other fields...
+
+    // Getters and Setters
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public int getField2() {
+        return field2;
+    }
+
+    public void setField2(int field2) {
+        this.field2 = field2;
+    }
+    // Add other getters and setters...
+}
